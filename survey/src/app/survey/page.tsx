a4c35413c27e42ded89983e3796469959cb20622
Bennie Liu <mingyeh1214@gmail.com>
2026-03-19 17:12:04 +0800

    更新

--- survey/src/app/survey/page.tsx
+++
@@ -1,9 +0,0 @@
-import SurveyForm from "@/components/SurveyForm";
-
-export default function SurveyPage() {
-  return (
-    <div className="min-h-screen bg-white">
-      <SurveyForm />
-    </div>
-  );
-}
